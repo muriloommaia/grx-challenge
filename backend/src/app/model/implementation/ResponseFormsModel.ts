06876a36ec42827d9d115ccd3e546b745c49d686
MuriloM <muriloommaia@gmail.com>
2022-05-03 20:35:41 -0300

    feat: implementa uuid no model

--- backend/src/app/model/implementation/ResponseFormsModel.ts
+++ backend/src/app/model/implementation/ResponseFormsModel.ts
@@ -1,10 +1,18 @@
-import { appendFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
+import { v4 } from 'uuid'
 import { IResponseFormDTO } from '../../ResponseForme/ResponseFormDTO'
 import { IAnswer } from '../IAnswer'
 import { IAnswerModel } from '../IAnswerModel'
-
 export class ResponseFormsModel implements IAnswerModel {
   async insertAnswer(answer: IResponseFormDTO & IAnswer): Promise<void> {
-    await appendFile('forms.json', JSON.stringify(answer))
+    console.log('testando')
+    const file = await readFile('forms.json', 'utf8')
+    const id = v4()
+    const data = JSON.parse(file)
+    const input = {
+      ...data,
+      [id]: answer
+    }
+    await writeFile('forms.json', JSON.stringify(input))
   }
 }
