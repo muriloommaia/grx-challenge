b22415a0883ae94309c6dd20c7395b80eba6afc9
MuriloM <muriloommaia@gmail.com>
2022-05-03 19:22:32 -0300

    fix: cria exportação de model

--- backend/src/app/model/IAnswerModel.ts
+++ backend/src/app/model/IAnswerModel.ts
@@ -1,7 +1,7 @@
 import { IResponseFormDTO } from '../ResponseForme/ResponseFormDTO'
 import { IAnswer } from './IAnswer'
 
-type IResponseToJSON = IResponseFormDTO & IAnswer
+export type IResponseToJSON = IResponseFormDTO & IAnswer
 export interface IAnswerModel {
   insertAnswer (answer: IResponseToJSON): Promise<void>
 }
