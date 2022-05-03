7de9ee8ab51f8172669715b3d53bec32ccc9028a
MuriloM <muriloommaia@gmail.com>
2022-05-03 17:09:15 -0300

    feat: cria pergunta 3

--- front-end/src/components/organisms/Forms.tsx
+++ front-end/src/components/organisms/Forms.tsx
@@ -1,4 +1,6 @@
+import { selectOptions } from '../../consts'
 import AnswerButton from '../molecules/AnswerButton'
+import SelectInput from '../molecules/SelectInput'
 
 export default function Forms() {
   return (
@@ -9,6 +11,9 @@ export default function Forms() {
       <AnswerButton numberAnswer={2}>
         2) Gosta de aprender com desafios?
       </AnswerButton>
+      <SelectInput numberAnswer={3} options={selectOptions}>
+        3) Gostaria de fazer parte da GRX?
+      </SelectInput>
     </div>
   )
 }
