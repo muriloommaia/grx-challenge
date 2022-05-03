5667af0f585d9d4b15af21af7ec5bd599ab434ea
MuriloM <muriloommaia@gmail.com>
2022-05-03 19:25:17 -0300

    fix: modifica entidade

--- backend/src/app/entities/Answers.ts
+++ backend/src/app/entities/Answers.ts
@@ -1,14 +1,14 @@
 import { IResponseToJSON } from '../model/IAnswerModel'
 
 export class Answers implements IResponseToJSON {
+  public readonly positiveQuantity: number
+  public readonly negativeQuantity: number
+  public readonly notAvailableQuantity: number
   constructor (
-    public answer1: string,
-    public answer2: string,
-    public answer3: string,
-    public answer4: string,
-    public positiveQuantity: number,
-    public negativeQuantity: number,
-    public notAvailableQuantity: number
+    public readonly answer1: string,
+    public readonly answer2: string,
+    public readonly answer3: string,
+    public readonly answer4: string
   ) {
     this.positiveQuantity = 0
     this.negativeQuantity = 0
