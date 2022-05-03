7f96e7524a7b5edca811affe74b175075d840fba
MuriloM <muriloommaia@gmail.com>
2022-05-03 20:04:26 -0300

    fix: ajusta tsconfig e entidade

--- backend/src/app/entities/Answers.ts
+++ backend/src/app/entities/Answers.ts
@@ -1,15 +1,18 @@
 import { IResponseToJSON } from '../model/IAnswerModel'
+import { IResponseFormDTO } from '../ResponseForme/ResponseFormDTO'
 
 export class Answers implements IResponseToJSON {
   public positiveQuantity: number
   public negativeQuantity: number
   public notAvailableQuantity: number
+  public answer1: string
+  public answer2: string
+  public answer3: string
+  public answer4: string
   constructor (
-    public answer1: string,
-    public answer2: string,
-    public answer3: string,
-    public answer4: string
+    props: IResponseFormDTO
   ) {
+    Object.assign(this, props)
     this.positiveQuantity = 0
     this.negativeQuantity = 0
     this.notAvailableQuantity = 0
