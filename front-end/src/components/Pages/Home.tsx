7421634ea7800a0828c4833831aaf362747ba532
MuriloM <muriloommaia@gmail.com>
2022-05-03 16:31:37 -0300

    feat: cria organismo forms e insere na home

--- front-end/src/components/Pages/Home.tsx
+++ front-end/src/components/Pages/Home.tsx
@@ -1,5 +1,10 @@
+import Forms from '../organisms/Forms'
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Desafio Coleta - GRX</h1>
+      <Forms />
+    </div>
   )
 }
---
+++ front-end/src/components/organisms/Forms.tsx
@@ -0,0 +1,11 @@
+import AnswerButton from '../molecules/AnswerButton'
+
+export default function Forms() {
+  return (
+    <div>
+      <AnswerButton numberAnswer={1}>
+        Pergunta 1
+      </AnswerButton>
+    </div>
+  )
+}
