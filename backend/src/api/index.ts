8b38bc69f5bbfb936f34353c526fba6555736828
MuriloM <muriloommaia@gmail.com>
2022-05-03 18:22:02 -0300

    feat: create server com express

---
+++ backend/src/api/index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import 'express-async-errors'
+
+class App {
+  public app: express.Application
+
+  constructor() {
+    this.app = express()
+    this.app.use(express.json())
+    this.addRouter()
+  }
+
+  public startServer(PORT: string | number = 3001): void {
+    this.app.listen(
+      PORT,
+      () => console.log(`Server running here 👉 http://localhost:${PORT}`)
+    )
+  }
+
+  public addRouter(): void {
+    // this.app.use(router)
+    // this.app.use(errorMiddleware)
+  }
+
+  public getApp(): express.Application {
+    return this.app
+  }
+}
+
+export default App
