3a6b82d3f62a366c650270a638b3766cbebef4b8
MuriloM <muriloommaia@gmail.com>
2022-05-03 17:30:52 -0300

    feat: create Submit Button

--- front-end/src/components/organisms/Forms.tsx
+++ front-end/src/components/organisms/Forms.tsx
@@ -1,11 +1,12 @@
 import { selectOptions } from '../../consts'
 import AnswerButton from '../molecules/AnswerButton'
 import SelectInput from '../molecules/SelectInput'
+import SubmitButton from '../molecules/SubmitButton'
 import TextArea from '../molecules/TextArea'
 
 export default function Forms() {
   return (
-    <div>
+    <form>
       <AnswerButton numberAnswer={1}>
         1) Você se considera bom em lógica?
       </AnswerButton>
@@ -18,6 +19,7 @@ export default function Forms() {
       <TextArea numberAnswer={4} max={200} min={15}>
         4) Por favor, justifique a resposta anterior
       </TextArea>
-    </div>
+      <SubmitButton />
+    </form>
   )
 }
