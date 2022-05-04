e4c48e91580863c2766b276dad41181454605218
MuriloM <muriloommaia@gmail.com>
2022-05-04 09:54:56 -0300

    feat: adiciona rota para download

--- backend/src/api/routes/route.ts
+++ backend/src/api/routes/route.ts
@@ -1,12 +1,26 @@
 import { Router } from 'express'
+import path from 'path'
 import { answerFactory } from '../../app/factory/answer.factory'
 
+const options = {
+  root: path.join('public'),
+  dotfiles: 'deny',
+  headers: {
+    'x-timestamp': Date.now(),
+    'x-sent': true
+  }
+}
 const router = Router()
 
 const controller = answerFactory()
 
 router.post('/form', async (request, response) => {
   await controller.handle(request, response)
+})
+
+
+router.get('/download/form', async (request, response) => {
+  response.sendFile('forms.json', options)
 })
 
 export { router }
