b2fa85deaaec9532a89dd950c0efadf516b7a78e
MuriloM <muriloommaia@gmail.com>
2022-05-03 23:21:38 -0300

    feat: renderização condicional na Home

--- front-end/src/components/Pages/Home.tsx
+++ front-end/src/components/Pages/Home.tsx
@@ -1,10 +1,14 @@
+import { useSelector } from 'react-redux'
+import { RootState } from '../../redux'
 import Forms from '../organisms/Forms'
+import Result from '../organisms/Result'
 import '../style/index.css'
 export default function Home() {
+  const { submit: { isSubmit } } = useSelector((state: RootState) => state)
   return (
     <div>
       <h1>Desafio Coleta - GRX</h1>
-      <Forms />
+      { !isSubmit ? <Forms /> : <Result /> }
     </div>
   )
 }
