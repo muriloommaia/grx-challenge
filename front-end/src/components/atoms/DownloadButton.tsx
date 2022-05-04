3970a87998c3637248ff71f452917df46ec7bd1a
MuriloM <muriloommaia@gmail.com>
2022-05-04 10:29:33 -0300

    fix: ajusta botão

--- front-end/src/components/atoms/DownloadButton.tsx
+++ front-end/src/components/atoms/DownloadButton.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 export default function DownloadButton() {
   return (
-    <div>
-      <a target={'_blank'} href="http://localhost:3001/download/form" download rel="noreferrer">
-        <button>Download</button>
+    <div className='flex flex-col items-center'>
+      <a
+
+        target={'_blank'}
+        href="http://localhost:3001/download/form"
+        download rel="noreferrer">
+        <button
+          className='relative p-3 bg-slate-400 mt-2 border-2 border-white rounded-md bottom-0'
+        >
+          Visualizar Formulários
+        </button>
       </a>
+      <p className='text-white text-xs'>
+        *Botão criado apenas para facilitar a visualização*
+      </p>
     </div>
   )
 }
