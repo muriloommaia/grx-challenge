9801bb9d7a6cf3fd0bae731bf6050d106bab456f
MuriloM <muriloommaia@gmail.com>
2022-05-04 00:16:17 -0300

    style: estiliza titulo

--- front-end/src/components/Pages/Home.tsx
+++ front-end/src/components/Pages/Home.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   const { submit: { isSubmit } } = useSelector((state: RootState) => state)
   return (
     <div className="flex flex-col items-center pt-4 w-10/12 md:min-w-[500px] md:w-5/12">
+      <div className='bg-white bg-opacity-50 px-4 rounded-3xl mb-2'>
       <h1 className='text-4xl py-3'>Desafio Coleta - GRX</h1>
+      </div>
       { !isSubmit ? <Forms /> : <Result /> }
     </div>
   )
