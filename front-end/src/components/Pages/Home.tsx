2c0d7174591b95101500b7ef15e0ccd787b2b06f
MuriloM <muriloommaia@gmail.com>
2022-05-03 23:57:31 -0300

    style: estiliza botoes

--- front-end/src/components/Pages/Home.tsx
+++ front-end/src/components/Pages/Home.tsx
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux'
 import { RootState } from '../../redux'
 import Forms from '../organisms/Forms'
 import Result from '../organisms/Result'
-import '../style/index.css'
 export default function Home() {
   const { submit: { isSubmit } } = useSelector((state: RootState) => state)
   return (
